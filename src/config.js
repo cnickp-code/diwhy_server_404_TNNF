21249cef05692b602f1037acca14ed0b332eaec7
Carlo Paredes <c.nick.paredes@gmail.com>
2020-08-04 12:26:40 -0700

    Configured app, config, server, added deps

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/diwhy',
+    TEST_DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/diwhy_test',
     NODE_ENV: process.env.NODE_ENV || 'development',
 }
