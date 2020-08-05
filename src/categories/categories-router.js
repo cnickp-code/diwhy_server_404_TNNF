c7d112ff2a58d2d71f3a6ccc556f9da0b9f41771
Carlo Paredes <c.nick.paredes@gmail.com>
2020-08-05 14:41:35 -0700

    Added categories router

--- src/categories/categories-router.js
+++ src/categories/categories-router.js
@@ -3,11 +3,39 @@ const CategoriesService = require('./categories-service');
 
 const categoriesRouter = express.Router();
 const { requireAuth } = require('../middleware/jwt-auth');
+const { restart } = require('nodemon');
 
 const bodyParser = express.json();
 
 categoriesRouter
     .route('/')
     .get((req, res, next) => {
+        const knexInstance = req.app.get('db');
+
+        CategoriesService.getAllCategories(knexInstance)
+            .then(categories => {
+                const serializedCategories = categories.map(category => {
+                    return CategoriesService.serializeCategoryItem(category);
+                })
+
+                res.status(200).json(serializedCategories);
+            })
+    })
+
+categoriesRouter
+    .route('/:category_id')
+    .get((req, res, next) => {
+        let knexInstance = req.app.get('db');
+
+        CategoriesService.getCategoryById(knexInstance, req.params.category_id)
+            .then(category => {
+                if(!category) {
+                    return res.status(404).json({
+                        error: { message: 'Date item does not exist.' }
+                    })
+                }
+                res.category = category;
+            })
         
+        res.status(200).json(CategoriesService.serializeCategoryItem(res.category));
     })
