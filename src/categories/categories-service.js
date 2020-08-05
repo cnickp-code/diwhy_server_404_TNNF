e57e484b0d734cd3095c8d330f45f1cb908047af
Carlo Paredes <c.nick.paredes@gmail.com>
2020-08-05 13:11:47 -0700

    Added categories service

--- src/categories/categories-service.js
+++ src/categories/categories-service.js
@@ -1,8 +1,25 @@
 const xss = require('xss');
 
-const CatgoriesService = {
+const CategoriesService = {
     getAllCategories(knex) {
         return knex
-            .select('')
+            .from('categories')
+            .select('*')
+            .orderBy('id')
+    },
+    getCategoryById(knex, id) {
+        return knex
+            .from('categories')
+            .select('*')
+            .where('id', id)
+            .first();
+    },
+    serializeCategoryItem(item) {
+        return {
+            id: item.id,
+            name: xss(item.name)
+        }
     }
 }
+
+module.exports = CategoriesService;
