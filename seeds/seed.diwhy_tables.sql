b7f199dcb000386125ad7b2294cd9cfeba5075ce
Carlo Paredes <c.nick.paredes@gmail.com>
2020-08-11 13:21:57 -0700

    Added seeds

--- seeds/seed.diwhy_tables.sql
+++ seeds/seed.diwhy_tables.sql
@@ -10,6 +10,10 @@ TRUNCATE
     posting_applicants
     RESTART IDENTITY CASCADE;
 
+INSERT INTO users (user_name, email, password)
+VALUES
+    ('Swaggatha', 'test@test.com', '$2a$12$nEuCrDTcCPllpYUAXoTHXudUicbfPFyYdfDonimIK./3tfivD7PrO');
+
 INSERT INTO categories(name)
 VALUES
     ('Woodworking'),
@@ -20,5 +24,17 @@ VALUES
     ('General Crafts'),
     ('Electronics'),
     ('Outdoorsmanship');
+
+INSERT INTO threads (title, user_id, category, date_created, content)
+VALUES
+    ('Test Thread 1', 1, 1, '2029-01-22T16:28:32.615Z', 'Test Content');
+
+INSERT INTO comments (user_id, thread_id, date_created, content)
+VALUES
+    (1, 1, '2029-01-22T16:28:32.615Z', 'Test Content');
+
+INSERT INTO postings (title, user_id, category, date_created, content)
+VALUES
+    ('Test Posting 1', 1, 1, '2029-01-22T16:28:32.615Z', 'Test Content');
 
 COMMIT;
