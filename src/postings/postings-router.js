1f55c586546c5754c051e93f10c021f4cd741fe8
Jon <jdog992@gmail.com>
2020-08-17 15:39:02 -0400

    adjusted posting

--- src/postings/postings-router.js
+++ src/postings/postings-router.js
@@ -22,10 +22,10 @@ postingsRouter
     })
     .post(jsonBodyParser, async (req, res, next) => {
         try {
-            const { title, content, user_id, category } = req.body
-
+            const { title, content, category } = req.body
+            const  user_id = req.user.id
             const newPosting = { title, content, user_id, category }
-
+            console.log('posting', newPosting)
             const insertedPosting = await PostingsService.insertPosting(
                 req.app.get('db'),
                 newPosting
